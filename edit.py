477c9d5c476104cf9e6aa6272d25f50e6b267a8d
V Wood <vwood.org@gmail.com>
2012-02-28 16:20:38 +1300

    Supports editing both functions and variables

--- edit.py
+++ edit.py
@@ -4,26 +4,47 @@ import tempfile
 import __main__
 
 defined = {}
-modeline = "#!/usr/bin/env python\n\n"
-line_no = 5
+boilerplate = "#!/usr/bin/env python\n\n"
 
 editor = os.environ.get('EDITOR', 'vim')
 
-def ed(name, g = globals()):
-	"""Edit a function definition in an editor."""
-	tmp = tempfile.NamedTemporaryFile()	
-	if defined.has_key(name):
-		tmp.write(defined[name])
-		tmp.flush()
-	else:
-		tmp.write(modeline + "def " + name + "():\n\t\"\"\"...\"\"\"\n\tpass\n")
-		tmp.flush()
-	os.system("vim %s +%s" % (tmp.name, str(line_no)))
+def edit(name, contents="", pos=0):
+	"""Edit contents in an external editor, starting at pos (defaults to 0).
+	The result is stored under name, and then evaluated in __main__."""
+	tmp = tempfile.NamedTemporaryFile()
+	tmp.write(contents)
 	tmp.flush()
+	os.system(editor + " %s +goto%s" % (tmp.name, str(pos)))
 	tmp.seek(0)
 	definition = tmp.read()
 	defined[name] = definition
 	exec definition in __main__.__dict__
+
+def var_skel(name):
+	return boilerplate + name + " = None\n" 
+
+def fun_skel(name):
+	return boilerplate + "def " + name + "():\n\t\"\"\"...\"\"\"\n\tpass\n"
+
+def var_start_pos(name):
+	return len(boilerplate) + len(name) + 4
+
+def fun_start_pos(name):
+	return len(boilerplate) + 4 + len(name) + 17
+
+def get_var(name):
+	return defined.get(name, var_skel(name))
+
+def get_fun(name):
+	return defined.get(name, fun_skel(name))
+
+def ved(name):
+	"""Edit a function definition in an editor."""
+	edit(name, get_var(name), var_start_pos(name))
+
+def fed(name):
+	"""Edit a function definition in an editor."""
+	edit(name, get_fun(name), fun_start_pos(name))
 
 def dump(filename = "dump.py"):
 	"""Dump definitions to a file."""
