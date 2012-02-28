441c859709df7362607991a15c8b08eeeecadf08
V Wood <vwood.org@gmail.com>
2012-02-28 13:10:50 +1300

    Initial Commit - Simple Editing of functions

---
+++ README.md
@@ -0,0 +1,4 @@
+REPLEdit.py
+===========
+
+Edit Python functions and data structures in your favourite CLI editor from the REPL!
---
+++ edit.py
@@ -0,0 +1,25 @@
+import sys
+import os
+import tempfile
+import __main__
+
+defined = {}
+modeline = "#!/usr/bin/env python\n\n"
+line_no = 5
+
+editor = os.environ.get('EDITOR', 'vim')
+
+def ed(name, g = globals()):
+	tmp = tempfile.NamedTemporaryFile()	
+	if defined.has_key(name):
+		tmp.write(defined[name])
+		tmp.flush()
+	else:
+		tmp.write(modeline + "def " + name + "():\n\t\"\"\"...\"\"\"\n\tpass\n")
+		tmp.flush()
+	os.system("vim %s +%s" % (tmp.name, str(line_no)))
+	tmp.flush()
+	tmp.seek(0)
+	definition = tmp.read()
+	defined[name] = definition
+	exec definition in __main__.__dict__
